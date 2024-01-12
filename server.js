c3e1be318e69c5b79a8f3804aedc51c2e1ea7638
Seven <phoenixpyra7@gmail.com>
2024-01-12 16:00:25 -0600

    fixed some file path heirarchy. pseudo code nnotes.

---
+++ server.js
@@ -0,0 +1,14 @@
+// first we need to set up the server boilerplate
+// imports (what do we need for this file?)
+// middleware (same as most projects)
+  // app.use(express.json())
+// html routes (how to serve the two html files?)
+// server listening (port and callback function)
+
+// we need to set up the crud operations
+  // writeToFile, readFromFile, deleteFromFile
+// using fs
+
+
+// then we need to set up the routes (from instructions)
+// implement the crud operations based on the request
