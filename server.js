26c6983fa608f309169e82324e972a2fc3d5ae3f
Seven <phoenixpyra7@gmail.com>
2024-01-12 17:31:40 -0600

    worked with instructor to set up initial files

--- server.js
+++ server.js
@@ -12,3 +12,31 @@
 
 // then we need to set up the routes (from instructions)
 // implement the crud operations based on the request.
+
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT ?? 3001;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+// API ROUTES
+app.get('/api/notes', (req, res) => {
+
+});
+
+app.post('/api/notes', (req, res) => {
+
+});
+
+// HTML SERVED FROM VIEWS AS ROUTES
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
